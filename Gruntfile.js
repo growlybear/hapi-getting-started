042c0c8c470c097511fc6f4d3413ea82331aff8e
Michael Allan <michaelallan@optushome.com.au>
2014-04-02 14:28:31 +1100

    Add grunt to project with default jshint task

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+module.exports = function (grunt) {
+
+    // configure
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        jshint: {
+            client: [
+                'Gruntfile.js'
+            ],
+        }
+
+        // browserify: {
+        //     options: {
+        //         transform: [ require('grunt-react').browserify ]
+        //     },
+        //     client: {
+        //         src: ['react_components/**/*.jsx'],
+        //         dest: 'js/dist/bundle.js'
+        //     }
+        // },
+
+        // watch: {
+        //     react: {
+        //         files: 'react_components/*.jsx',
+        //         tasks: ['browserify']
+        //     }
+        // }
+    });
+
+    // load
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    // grunt.loadNpmTasks('grunt-contrib-watch');
+    // grunt.loadNpmTasks('grunt-browserify');
+
+    // grunt.loadTasks('tasks');
+
+    // register
+    grunt.registerTask('default', ['jshint']);
+};
