559af18fd97b113968e120112768559772b833b8
Michael Allan <michaelallan@optushome.com.au>
2014-04-02 16:55:39 +1100

    Add /hello route with Joi validations

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 var Hapi = require('hapi');
+var Joi = require('joi');
 
 var server = new Hapi.Server(3000, 'localhost');
+
+// TODO refactor this config into separate modules
+var helloConfig = {
+    handler: function (request, reply) {
+        console.log(request);
+
+        var names = request.params.name.split('/');
+
+        reply({
+            first: names[0],
+            last: names[1],
+            mood: request.query.mood
+        });
+    },
+    validate: {
+        path: {
+            name: Joi.string().min(8).max(100)
+        },
+        query: {
+            mood: Joi.string().valid(['neutral', 'happy', 'sad']).default('neutral')
+        }
+    }
+};
 
 server.route({
     path: '/',
@@ -10,6 +34,12 @@ server.route({
         reply('Hello Hapi world');
     }
 });
+
+server.route({
+    path: '/hello/{name*2}',
+    method: 'GET',
+    config: helloConfig
+})
 
 server.start(function () {
     console.log('Hapi server started @ ' + server.info.uri);
